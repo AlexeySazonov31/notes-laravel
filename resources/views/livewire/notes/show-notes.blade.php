39048a05b46fa7be64f85cfa88fcfc38f83c043a
Aliaksei Sazonau <sazonleha010203@gmail.com>
2024-08-09 23:03:11 +0300

    create note and notes page

--- resources/views/livewire/notes/show-notes.blade.php
+++ resources/views/livewire/notes/show-notes.blade.php
@@ -12,9 +12,48 @@ new class extends Component {
 }; ?>
 
 <div>
-    <ul>
-        @foreach ($notes as $note)
-            <li>{{ $note->title }}</li>
-        @endforeach
-    </ul>
+    <div class="space-y-2">
+        @if ($notes->isEmpty())
+            <div class="text-center">
+                <p class="text-xl font-bold">No notes yet</p>
+                <p class="text-sm">Let's create your first note to send</p>
+                <x-button class="mt-6" primary icon-right="plus" href="{{ route('notes.create') }}" wire:navigate>
+                    Create note
+                </x-button>
+            </div>
+        @else
+            <x-button class="mb-12" primary icon-right="plus" href="{{ route('notes.create') }}" wire:navigate>
+                Create note
+            </x-button>
+            <div class="grid grid-cols-2 gap-4 mt-12">
+                @foreach ($notes as $note)
+                    <x-card wire:key='{{ $note->id }}'>
+                        <div class="flex justify-between">
+                            <div>
+                                <a href="#" class="text-xl font-bold hover:underline hover:text-blue-500">
+                                    {{ $note->title }}
+                                </a>
+                                <p>{{ Str::limit($note->body, 100) }}</p>
+                            </div>
+                            <div class="text-xs text-gray-500">
+                                {{ \Carbon\Carbon::parse($note->send_date)->format('M d, Y') }}
+                            </div>
+                        </div>
+                        <div class="flex items-end justify-between mt-4 space-x-1">
+                            <p class="text-xs">
+                                Recipient:
+                                <span class="font-semibold">
+                                    {{ $note->recipient }}
+                                </span>
+                            </p>
+                            <div>
+                                <x-mini-button class="border" rounded icon="eye" flat gray interaction="primary" />
+                                <x-mini-button class="border" rounded icon="trash" flat gray interaction="negative" />
+                            </div>
+                        </div>
+                    </x-card>
+                @endforeach
+            </div>
+        @endif
+    </div>
 </div>
